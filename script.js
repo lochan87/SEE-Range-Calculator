29b96dc762f9153d922b07a1c3a584dafcfe8a5b
Lochan T N <145211584+lochan87@users.noreply.github.com>
2024-08-01 20:23:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 if ('serviceWorker' in navigator) {
     const blob = new Blob([`
-        // Service Worker Script
+       // Service Worker Script
+
+        const CACHE_NAME = 'my-cache-v1';
+        const urlsToCache = [
+            'https://i.ibb.co/gb0GBZr/Designer.jpg',
+            // Add other images or assets you want to cache
+        ];
 
         // Install event
         self.addEventListener('install', event => {
             event.waitUntil(
-                caches.open('my-cache').then(cache => {
-                    return cache.addAll([
-                        'https://i.ibb.co/gb0GBZr/Designer.jpg'
-                    ]);
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        return cache.addAll(urlsToCache);
+                    })
+            );
+        });
+
+        // Activate event
+        self.addEventListener('activate', event => {
+            event.waitUntil(
+                caches.keys().then(cacheNames => {
+                    return Promise.all(
+                        cacheNames.map(cacheName => {
+                            if (cacheName !== CACHE_NAME) {
+                                return caches.delete(cacheName);
+                            }
+                        })
+                    );
                 })
             );
         });
@@ -17,7 +37,12 @@ if ('serviceWorker' in navigator) {
         self.addEventListener('fetch', event => {
             event.respondWith(
                 caches.match(event.request).then(response => {
-                    return response || fetch(event.request);
+                    return response || fetch(event.request).then(fetchResponse => {
+                        return caches.open(CACHE_NAME).then(cache => {
+                            cache.put(event.request, fetchResponse.clone());
+                            return fetchResponse;
+                        });
+                    });
                 })
             );
         });
